b94565c56ece1a64972e4cf923d9a43aa77ab022
Shai <Shaisisso1@gmail.com>
2022-06-20 00:47:25 +0300

    API URL from aws

--- src/utility/Utils.js
+++ src/utility/Utils.js
@@ -1,2 +1,2 @@
 //export const WEB_URL = "http://localhost:8080";
-export const WEB_URL = "http://smartfood.us-east-1.elasticbeanstalk.com";
+export const WEB_URL = "http://smartfood.us-east-1.elasticbeanstalk.com:5000";
