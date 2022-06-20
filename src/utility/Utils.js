dd70f9a0f4fde257f07451f9e008c1a4cc669203
Shai <Shaisisso1@gmail.com>
2022-06-20 23:23:30 +0300

    new api url

--- src/utility/Utils.js
+++ src/utility/Utils.js
@@ -1,3 +1,3 @@
 export const SERVER_PORT = "5000"
 //export const WEB_URL = `http://localhost:${SERVER_PORT}`;
-export const WEB_URL = "http://smartfood.us-east-1.elasticbeanstalk.com";
+export const WEB_URL = "http://smartfood-env.eba-qmkz53pt.eu-central-1.elasticbeanstalk.com";
