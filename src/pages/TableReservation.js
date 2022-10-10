e669bd8f1f08508d07363390f5af500571bb32f1
Shai <Shaisisso1@gmail.com>
2022-10-10 12:11:24 +0300

    Min and max dates in TableReservation

--- src/pages/TableReservation.js
+++ src/pages/TableReservation.js
@@ -1,70 +1,62 @@
 import React, { useState } from "react";
-import PropTypes from 'prop-types';
-import Dropdown from "react-bootstrap/Dropdown";
 import Form from 'react-bootstrap/Form';
-
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import { formatDate } from "../utility/Utils";
+//import DatePicker from "react-datepicker";
+//import "react-datepicker/dist/react-datepicker.css";
 
 const hoursList = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
+
 const getCurrentDate = () => {
+    return formatDate(new Date())
+}
+const getMaxDate = () => {
     let date = new Date();
-    let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
-    let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date);
-    let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
-    console.log(`${da}-${mo}-${ye}`);
-    return `${ye}-${mo}-${da}`
+    date.setFullYear(date.getFullYear()+1)
+    return formatDate(date)
 }
 
-
-const TableReservation = props => {
-    const [startDate, setStartDate] = useState(new Date());
-    const [showDate, setDateClick] = useState(false);
+const TableReservation =()=> {
     const [chosenDate, setChosenDate] = useState(getCurrentDate());
 
     const onSubmit = () => {
         console.log(`${getCurrentDate()}`)
-       // console.log(new Date().toLocaleDateString("en-US"))
+        // console.log(new Date().toLocaleDateString("en-US"))
         //console.log(`${new Date().toLocaleDateString()}`)
     }
-
+    const onChangeDate=(event)=>{
+        setChosenDate(event.target.value)
+    }
     return (
-        <div className="container col col-lg-6 col-sm-10">
-            <form role="form" onSubmit={onSubmit}>
+        <div className="container col col-lg-6 col-sm-10 p-3" style={{ backgroundColor: "#ffffff60", }}>
+            <div className="section-title">
+                <h1 style={{ color: "black" }}>Table Reservation</h1>
+            </div>
+            <form onSubmit={onSubmit}>
                 <div className="row">
                     <div className="col-md-6 form-group">
-                        <input type="text" name="name" className="form-control" placeholder="First Name" required />
+                        <input type="text" name="name" className="form-control" placeholder="*First Name" required />
                     </div>
                     <div className="col-md-6 form-group mt-3 mt-md-0">
-                        <input type="text" name="name" className="form-control" placeholder="Last Name" required />
+                        <input type="text" name="name" className="form-control" placeholder="*Last Name" required />
+                    </div>
+                </div>
+                <div className="row form-group mt-3">
+                    <div className="col-md-6 ">
+                        <input type="phone" className="form-control" name="subject" placeholder="*Phone Number" required />
+                    </div>
+                    <div className="col-md-6 ">
+                        <input type="email" className="form-control" name="email" id="email" placeholder=" Email"/>
                     </div>
                 </div>
                 <div className="form-group mt-3">
-                    <input type="phone" className="form-control" name="subject" placeholder="Phone Number" required />
-                </div>
-                <div className="form-group mt-3">
-                    <input type="email" className="form-control" name="email" id="email" placeholder=" Email" required />
-                </div>
-                <div className="form-group mt-3">
-                    {/* {
-                        showDate ?
-                            <DatePicker
-                                selected={startDate}
-                                onChange={(date) => setStartDate(date)}
-                                minDate={new Date()}
-                                //  maxDate={addMonths(new Date(), 5)}
-                                showDisabledMonthNavigation
-                                inline
-                            />
-                            :
-                            <input type="date" className="form-control" name="date" placeholder="date" required onClick={() => setDateClick(true)} />
-                    } */}
                     <div className="row">
                         <div className="col-md-6 form-group">
-                            <input type="date" style={{ textAlign: "left" }} className="form-control" name="date" value={chosenDate} onChange={setChosenDate} required />
+                            <input type="date" style={{ textAlign: "left" }} className="form-control" 
+                            name="date" value={chosenDate} onChange={onChangeDate} required 
+                            min={formatDate(new Date())} max={getMaxDate()} />
                         </div>
                         <div className="col-md-6 form-group mt-3 mt-md-0">
-                        <Form.Select aria-label="Default select example" >
+                            <Form.Select aria-label="Default select example" >
                                 <option disabled>Choose Hour</option>
                                 {
                                     hoursList.map((item, key) => (
@@ -91,10 +83,6 @@ const TableReservation = props => {
         </div>
 
     );
-};
-
-TableReservation.propTypes = {
-
 };
 
 export default TableReservation;
--- src/utility/Utils.js
+++ src/utility/Utils.js
@@ -8,3 +8,11 @@
 // --- Local Environment --- //
 export const SERVER_PORT = "8080"
 export const API_URL = `http://localhost:${SERVER_PORT}`;
+
+export function formatDate(date){
+    let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+    let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date);
+    let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
+    console.log(`${da}-${mo}-${ye}`);
+    return `${ye}-${mo}-${da}`
+}
