3b2b8af4d2c623b9c53c35783ffd576ace096c80
Shai <Shaisisso1@gmail.com>
2022-10-10 15:35:57 +0300

    Get categories from server and change them to valid string

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,11 +1,11 @@
 import BackgroundImg from '../assets/background.jpg'
 import { useEffect, useRef, useState } from 'react';
 import Axios from 'axios';
-import {  API_URL } from '../utility/Utils';
+import { API_URL } from '../utility/Utils';
 
-const categories = ['Starters', 'Salads', 'Main Dishes']
 
 const MenuPage = () => {
+    const [categories, setCategories] = useState([])
     const [menu, setMenu] = useState([])
     const [dishesToDisplay, setDishesToDisplay] = useState(menu);
     const [radioValue, setRadioValue] = useState(-1);
@@ -14,23 +14,39 @@ const MenuPage = () => {
     //     setMenu(res.data)
     //     setDishesToDisplay(res.data)
     // }
-
-    const getData = ()=>{
+    const getCategories = () => {
+        Axios.get(`${API_URL}/api/menu/categories`)
+            .then(res => {
+                setCategories(res.data)
+            }).catch(err => {
+                // TODO: Change to popup error message
+                alert('Error')
+            })
+    }
+    const getData = () => {
         Axios.get(`${API_URL}/api/menu`)
-        .then(res=>{
-            setMenu(res.data)
-            setDishesToDisplay(res.data)
-        }).catch(err=>{
-            console.log(`${API_URL}/api/menu`)
-            console.log(err)
-            alert('Error')
-        })
+            .then(res => {
+                let data = res.data.map(item => {
+                    let category = item.category.split("_")
+                        .map(word => word[0].toUpperCase() + word.substring(1).toLowerCase())
+                        .join(" ")
+                    item.category = category
+                    return item
+                })
+                setMenu(data)
+
+                setDishesToDisplay(data)
+            }).catch(err => {
+                // TODO: Change to popup error message
+                alert('Error')
+            })
     }
 
     const mounted = useRef();
     useEffect(() => {
         if (!mounted.current) {
             mounted.current = true;
+            getCategories();
             getData();
         }
     });
