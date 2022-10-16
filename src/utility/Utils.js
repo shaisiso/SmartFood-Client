859e6909d441f70038928d384ce5f36c1e8bab17
Shai <Shaisisso1@gmail.com>
2022-10-16 12:02:05 +0300

    change AWS Server api url

--- src/utility/Utils.js
+++ src/utility/Utils.js
@@ -3,11 +3,11 @@
 //export const WEB_URL = `http://localhost:${SERVER_PORT}`;
 
 // --- AWS Server --- //
-// export const API_URL = "http://smartfood-env.eba-qmkz53pt.eu-central-1.elasticbeanstalk.com";
+export const API_URL = "http://smartfood.eu-central-1.elasticbeanstalk.com";
 
 // --- Local Environment --- //
-export const SERVER_PORT = "8080"
-export const API_URL = `http://localhost:${SERVER_PORT}`;
+// export const SERVER_PORT = "8080"
+// export const API_URL = `http://localhost:${SERVER_PORT}`;
 
 export function formatDate(date){
     let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
